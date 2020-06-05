e75541db90ec71e3b431d07db6af78dbc85af6c0
razumovskiy <kolosovp94@gmail.com>
2020-06-05 03:35:50 +0200

    initial commit

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SmartphoneShop.Models;
+
+namespace SmartphoneShop.Data
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace SmartphoneShop.Models
+{
+    public class User: IdentityUser
+    {
+        public int Year { get; set; }
+    }
+}
