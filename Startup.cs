b52f1031a64390341a947091de4690c2e345edf2
razumovskiy <kolosovp94@gmail.com>
2020-06-05 05:04:20 +0200

    roles management is done

--- Startup.cs
+++ Startup.cs
@@ -30,10 +30,19 @@ namespace SmartphoneShop
         {
             services.AddDbContext<ApplicationContext>(options =>
                 options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
- 
-            services.AddIdentity<User, IdentityRole>()
+
+            services.AddIdentity<User, IdentityRole>(opts =>
+            {
+                opts.Password.RequiredLength = 5;   // минимальная длина
+                opts.Password.RequireNonAlphanumeric = false;   // требуются ли не алфавитно-цифровые символы
+                opts.Password.RequireLowercase = false; // требуются ли символы в нижнем регистре
+                opts.Password.RequireUppercase = false; // требуются ли символы в верхнем регистре
+                opts.Password.RequireDigit = false; // требуются ли цифры
+                opts.User.RequireUniqueEmail = true;    // уникальный email
+                opts.User.AllowedUserNameCharacters = ".@abcdefghijklmnopqrstuvwxyz"; // допустимые символы
+            })
                 .AddEntityFrameworkStores<ApplicationContext>();
-                 
+
             services.AddControllersWithViews();
             services.AddControllersWithViews();
         }
